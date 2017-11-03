a9b6c1eb8b59b040f93bebf3e310d2a2eb2d3dd6
Lu, Alisa <alu@lcs-mba3124.lcs.on.ca>
2017-11-03 10:49:34 -0400

    first

--- 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -1,82 +1,16 @@
-//: [Next](@next)
-//: # A Blank Canvas
-//:
-//: Use this page to experiment. Have fun!
-/*:
- ## Required code
- 
- The following statements are required to make the playground run.
- 
- Please do not remove.
- */
-import Cocoa
-import PlaygroundSupport
 
-// Create canvas
-let canvas = Canvas(width: 500, height: 500)
 
-canvas.defaultBorderWidth = 5
+// Use an integer
+var x : Double = 10.5
 
-for x in stride (from: 25, through: 475, by: 50) {
-    for y in stride(from: 475, through: 25, by: -50) {
-        // white
-        canvas.fillColor = Color.white
-        canvas.drawEllipse(centreX: x, centreY: y, width: 25, height: 25)
-        
-        let randomvalue = random(from: 1, toButNotIncluding: 5)
-        // black
-        canvas.fillColor = Color.black
-        canvas.drawEllipse(centreX: x + 10, centreY: y, width: 25, height: 25)
-        
-        canvas.fillColor = Color.black
-        canvas.drawEllipse(centreX: x - 10, centreY: y, width: 25, height: 25)
-        
-        canvas.fillColor = Color.black
-        canvas.drawEllipse(centreX: x, centreY: y + 10, width: 25, height: 25)
-        
-        canvas.fillColor = Color.black
-        canvas.drawEllipse(centreX: x, centreY: y - 10, width: 25, height: 25)
-        
-        
-    }
-    
-}
+// Var y
+var y = 7
+
+//  The value of x
+x = 5.2
 
 
 
 
 
-
-
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-
-
-
-
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
-// Don't remove the code below
-PlaygroundPage.current.liveView = canvas.imageView
 
