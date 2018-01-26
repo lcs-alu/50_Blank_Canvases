44fbc2951204d21577a2b5678142e92c62200055
Lu, Alisa <alu@lcs-mba3124.lcs.on.ca>
2018-01-26 15:35:56 -0500

    all changes

--- 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -1,24 +1,14 @@
-//: [Next](@next)
-//: # A Blank Canvas
-//:
-//: Use this page to experiment. Have fun!
-/*:
- ## Required code
- 
- The following statements are required to make the playground run.
- 
- Please do not remove.
- */
-import Cocoa
-import PlaygroundSupport
 
-// Create canvas
-let canvas = Canvas(width: 400, height: 300)
 
+// Use an integer
+var x : Double = 10.5
 
  // Draw eliipse
 canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
 // Don't remove the code below
 PlaygroundPage.current.liveView = canvas.imageView
+
+
+
 
 
